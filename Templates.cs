526163d82e5fc720130dbd1da6fae899274ec32f
John Berberian <53021080+LindirQuenya@users.noreply.github.com>
2022-03-05 17:56:20 -0500

    Put DB-loading on another thread, fix braces
    Author: @LindirQuenya
    Repository: https://github.com/LindirQuenya/SharpLauncher
    
    - [29cc43a] Ignore a bunch of stuff.
    - [06fa08d] Add a ton of braces and turn one if/else into a ternary operator statement.
    - [7874664] Restructure, minimize database queries.
    - [29a9b2f] Bugfix, and integrate changes from 079555e.
    - [1951b1f] Optimize imports, ensure that all queryCache accesses are thread-safe.
    - [2ee26e9] Put DB loading on its own thread.

---
+++ Templates.cs
@@ -0,0 +1,66 @@
+﻿namespace SharpLauncher
+{
+    // A class for holding metadata about a game.
+    public class MetaDataObj
+    {
+        // This maps "internal" database column-names to "external" column-names.
+        // TODO: use this for translations.
+        public static readonly Dictionary<string, string> metadataFields = new()
+        {
+            { "title", "Title" },
+            { "alternateTitles", "Alternate Titles" },
+            { "series", "Series" },
+            { "developer", "Developer" },
+            { "publisher", "Publisher" },
+            { "source", "Source" },
+            { "releaseDate", "Release Date" },
+            { "platform", "Platform" },
+            { "version", "Version" },
+            { "library", "Library" },
+            { "tagsStr", "Tags" },
+            { "language", "Language" },
+            { "playMode", "Play Mode" },
+            { "status", "Status" },
+            { "activeDataOnDisk", "Format" },
+            { "notes", "Notes" },
+            { "originalDescription", "Original Description" }
+        };
+        public string Title { get; set; } = "";
+        public string AlternateTitles { get; set; } = "";
+        public string Series { get; set; } = "";
+        public string Developer { get; set; } = "";
+        public string Publisher { get; set; } = "";
+        public string Source { get; set; } = "";
+        public string ReleaseDate { get; set; } = "";
+        public string Platform { get; set; } = "";
+        public string Version { get; set; } = "";
+        public string Library { get; set; } = "";
+        public string Tags { get; set; } = "";
+        public string Language { get; set; } = "";
+        public string PlayMode { get; set; } = "";
+        public string Status { get; set; } = "";
+        public string Format { get; set; } = "";
+        public string Notes { get; set; } = "";
+        public string OriginalDescription { get; set; } = "";
+
+    }
+
+    // Template for list items
+    public class QueryItem
+    {
+        public string Title { get; set; } = "";
+        public string Developer { get; set; } = "";
+        public string Publisher { get; set; } = "";
+        public string ID { get; set; } = "";
+        public string tagsStr { get; set; } = "";
+    }
+
+    // Template for Add-Apps
+    public class AddApp
+    {
+        public string ID { get; set; } = "";
+        public string ParentGameId { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string ApplicationPath { get; set; } = "";
+    }
+}
