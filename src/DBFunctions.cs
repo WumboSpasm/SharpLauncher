69633e4a1d3bffeb25af86eb3b6d3ec2e116a889
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-01-04 23:59:29 -0500

    Clean up using directives

--- src/DBFunctions.cs
+++ src/DBFunctions.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Common;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+
 using Microsoft.Data.Sqlite;
 
 namespace SharpLauncher
